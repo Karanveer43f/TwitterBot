a2e4dd04ad1dc52b8d79ba8c1b03da62b0fb9d03
Karanveer <karanveer43f@gmail.com>
2023-04-09 23:44:49 +0530

    created twitter client

---
+++ twitterClient.js
@@ -0,0 +1,15 @@
+const {TwiiterApi } = require('twitter-api-v2');
+
+const client = new TwiiterApi({
+    appkey : process.env.API_KEY,
+    appSecret : process.env.API_SECRET,
+    accessToken : process.env.ACCESS_TOKEN,
+    accessSecret : process.env.ACCESS_SECRET,
+});
+
+const bearer = new TwitterApi(process.env.BEARER_TOKEN);
+
+const twitterClient = client.readWrite;
+const twitterBearer = bearer.readOnly;
+
+module.exports = {twitterClient , twitterBearer};
