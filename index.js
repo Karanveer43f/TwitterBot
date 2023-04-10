0b41b78a295c39f3531f978cba00a5f22cf0ba75
Karanveer <karanveer43f@gmail.com>
2023-04-10 14:03:05 +0530

    our bot can now tweet with photos

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const { twitterClient } = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
+const { download } = require("./utilities");
 
 app.get("/", (req, res) => {
   res.send("Check the tweets done on @karanveer43fp on twitter.com");
@@ -14,12 +15,22 @@ app.listen(port, () => {
 });
 
 const tweet = async () => {
-  let number = Math.random();
-  try {
-    await twitterClient.v2.tweet(`I am random ${number} tweet`);
-  } catch (e) {
-    console.log(e);
-  }
+  const uri = "https://i.imgur.com/bDcJmR4.jpeg";
+  const filename = "image.png";
+
+  download(uri, filename, async function () {
+    try {
+      const mediaId = await twitterClient.v1.uploadMedia("./image.png");
+      await twitterClient.v2.tweet({
+        text: "Heyy guys, look at the spectacular Milky-way Galaxy",
+        media: {
+          media_ids: [mediaId],
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
 };
 
 const cronTweet = new CronJob("30 * * * * *", async () => {
